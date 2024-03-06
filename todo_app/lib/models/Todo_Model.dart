ea0ce57c42e0ea7907a3ab6a97db65307285894e
Ezzat Effat <156718898+Ezzat200@users.noreply.github.com>
2024-03-07 00:24:25 +0200

    create Todo Model

---
+++ todo_app/lib/models/Todo_Model.dart
@@ -0,0 +1,12 @@
+class TodoModel {
+  final String title;
+  final String subTitel;
+  final String date;
+  final int color;
+
+  TodoModel(
+      {required this.title,
+      required this.subTitel,
+      required this.date,
+      required this.color});
+}

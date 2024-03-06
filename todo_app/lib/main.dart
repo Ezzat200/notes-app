7485630247de52ada801130606dd5677d746cd73
Ezzat Effat <156718898+Ezzat200@users.noreply.github.com>
2024-03-06 21:05:23 +0200

    change them and crate todo page

--- todo_app/lib/main.dart
+++ todo_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/views/Todo_app.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,9 +14,9 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-       
+       brightness: Brightness.dark
       ),
-      home:  Container(),
+      home: const Todo(),
     );
   }
 }
